8a8736e7f88e6953f1c80101d8f3071cf2d291fb
marcelofury <marcelofury13579@gmail.com>
2025-06-26 17:42:19 +0300

    Added new dates and math object

---
+++ Dates/newDate().js
@@ -0,0 +1,12 @@
+new Date()
+    // Creates a new Date object representing the current date and time
+
+
+new Date(year, month)
+new Date(year, month, day)
+new Date(year, month, day, hours)
+new Date(year, month, day, hours, minutes)
+new Date(year, month, day, hours, minutes, seconds)
+new Date(year, month, day, hours, minutes, seconds, ms)
+
+new Date(milliseconds)
---
+++ Math object/math.method().js
@@ -0,0 +1,11 @@
+Math.round(x) //Returns x rounded to its nearest integer
+Math.ceil(x) //Returns x rounded up to its nearest integer
+Math.floor(x) //Returns x rounded down to its nearest integer
+Math.trunc(x) //Returns the integer part of x(new in ES6)
+Math.sign(x) //Returns the sign of x, indicating whether x is positive, negative or zero(new in ES6)
+Math.cbrt(x) //Returns the cube root of x(new in ES6) 
+Math.sqrt(x) //Returns the square root of x
+Math.pow(x, y) //Returns the value of x to the power of y
+Math.exp(x) //Returns the value of Ex
+Math.log(x) //Returns the natural logarithm (base E) of x 
+Math.random() //Returns a random number between 0 and 1
